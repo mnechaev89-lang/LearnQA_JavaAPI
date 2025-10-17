a727fb068d8d7a5126d658a5b7bc5fff5c549bfc
mnech <mnechaev89@mail.ru>
2025-10-17 12:55:55 +0300

    Merge remote-tracking branch 'origin/main'

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,10 +1,16 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
 
     @Test
     public void testHelloWorld(){
-        String name = "Максим";
-        System.out.println("Hello World" + " " + name);
+        Response resp = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        resp.prettyPrint();
+
     }
 }
