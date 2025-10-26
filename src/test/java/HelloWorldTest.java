ef889d7862dd712a1122c4c1d28f07cc962e8041
mnech <mnechaev89@mail.ru>
2025-10-26 14:53:17 +0300

    Merge remote-tracking branch 'origin/main'

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,21 +1,24 @@
 import io.restassured.RestAssured;
-import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
+
 public class HelloWorldTest {
 
     @Test
     public void testRestAssured(){
+        Response resp = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
 
-
-        JsonPath resp = RestAssured
-                .get("https://playground.learnqa.ru/api/get_json_homework")
-                .jsonPath();
-        String message2 = resp.get("messages[1].message");
-        System.out.println(message2);
-
+        String redirectUrl = resp.getHeader("Location");
+        System.out.println("Редиректит на URL : " + redirectUrl);
     }
 }
