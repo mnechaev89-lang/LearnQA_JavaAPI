c76a5c9098cd0ccd0a96bac9a057e81684e1e295
mnech <mnechaev89@mail.ru>
2025-11-04 20:21:44 +0300

    TestCookieValue

--- src/test/java/StringSizeTest.java
+++ src/test/java/StringSizeTest.java
@@ -1,14 +1,30 @@
+import io.restassured.RestAssured;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringSizeTest {
 
     @Test
-    public void testStringSize() {
+    public void testHeaderValue() {
+        Map<String, String> headers = new HashMap<>();
 
-        String sentence = "Hello, world";
-        int size = 15;
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/homework_header")
+                .andReturn();
 
-        assertEquals(size, sentence.length());
+        //Headers responseHeader = response.getHeaders();
+        //System.out.println("Headers response: " + "\n" + responseHeader);
+        String headerValue = response.getHeader("x-secret-homework-header");
+        System.out.println("Значение заголовка 'x-secret-homework-header': " + headerValue);
+
+        assertEquals("Some secret value", headerValue, "Header x-secret-homework-header должна быть 'Some secret value'");
+
+
     }
 }
