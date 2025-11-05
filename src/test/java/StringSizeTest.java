48477caa467b8f5a21afb7f620eb4cc95c0b5df2
mnech <mnechaev89@mail.ru>
2025-11-05 13:23:07 +0300

    testCookieValue

--- src/test/java/StringSizeTest.java
+++ src/test/java/StringSizeTest.java
@@ -1,30 +1,21 @@
 import io.restassured.RestAssured;
-import io.restassured.http.Headers;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StringSizeTest {
 
     @Test
-    public void testHeaderValue() {
-        Map<String, String> headers = new HashMap<>();
+        public void testCookieValue() {
 
-        Response response = RestAssured
-                .get("https://playground.learnqa.ru/api/homework_header")
-                .andReturn();
+            Response response = RestAssured
+                    .get("https://playground.learnqa.ru/api/homework_cookie")
+                    .andReturn();
 
-        //Headers responseHeader = response.getHeaders();
-        //System.out.println("Headers response: " + "\n" + responseHeader);
-        String headerValue = response.getHeader("x-secret-homework-header");
-        System.out.println("Значение заголовка 'x-secret-homework-header': " + headerValue);
-
-        assertEquals("Some secret value", headerValue, "Header x-secret-homework-header должна быть 'Some secret value'");
-
-
+            //System.out.println(response.getCookies());
+            String nameCookie = "HomeWork";
+            String valueCookie = "hw_value";
+            assertTrue(response.getCookies().containsKey(nameCookie), "Отсутствует cookie 'HomeWork'");
+            assertTrue(response.getCookies().containsValue(valueCookie), "Отсутствует значение 'hw_value' у куки 'HomeWork'");
     }
 }
