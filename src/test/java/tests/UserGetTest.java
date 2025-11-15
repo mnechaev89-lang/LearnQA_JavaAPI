23d0bd473537c93265a5dab58d88183540453a22
mnech <mnechaev89@mail.ru>
2025-11-15 18:57:46 +0300

    Запрос данных другого пользователя

--- src/test/java/tests/UserGetTest.java
+++ src/test/java/tests/UserGetTest.java
@@ -1,15 +1,24 @@
 package tests;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import lib.BaseTestCase;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import lib.Assertions;
+import lib.ApiCoreRequests;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Epic("User data cases")
+@Feature("User Data Retrieval")
+
 public class UserGetTest extends BaseTestCase {
+    private final ApiCoreRequests apiCoreRequests = new ApiCoreRequests();
 
     @Test
     public void testGetUserDataNotAuth() { // неавторизованный пользователь (только username)
@@ -49,5 +58,36 @@ public class UserGetTest extends BaseTestCase {
         String[] expectedFields = {"username", "firstName", "lastName", "email"};
         Assertions.assertJsonHasFields(responseUserData, expectedFields); // проверяем, что авторизованный пользователь получает все данные
     }
+
+    @Test
+    @Description("This test verifies authorization with another id user")
+    @DisplayName("Authorization test with other id")
+    public void testGetUserDetailsAuthAsOtherUser() {
+        Map<String, String> authData = new HashMap<>();
+        authData.put("email", "vinkotov@example.com");
+        authData.put("password", "1234");
+
+        Response responseGetAuth = apiCoreRequests.makePostRequest(
+                "https://playground.learnqa.ru/api/user/login",
+                authData
+                );
+        String header = this.getHeader(responseGetAuth, "x-csrf-token"); // получаем header и cookie
+        String cookie = this.getCookie(responseGetAuth, "auth_sid");
+
+        Response responseUserData = apiCoreRequests.makeGetRequest(
+                "https://playground.learnqa.ru/api/user/1",  // ← Другой ID (не 2)
+                header,
+                cookie
+        );
+        //System.out.println(responseUserData.asString());
+        Assertions.assertJsonHasField(responseUserData, "username");
+        Assertions.assertJsonHasNotField(responseUserData, "firstName");
+        Assertions.assertJsonHasNotField(responseUserData, "lastName");
+        Assertions.assertJsonHasNotField(responseUserData, "email");
+
+
+    }
+
+
 
 }
