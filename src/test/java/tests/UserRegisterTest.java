099173cb5f17574e5ced4c7edbc79204f1dd4271
mnech <mnechaev89@mail.ru>
2025-11-15 14:49:42 +0300

    Тесты на метод user

--- src/test/java/tests/UserRegisterTest.java
+++ src/test/java/tests/UserRegisterTest.java
@@ -1,16 +1,29 @@
 package tests;
-
+import io.qameta.allure.Description;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import lib.Assertions;
 import lib.BaseTestCase;
 import lib.DataGenerator;
+import lib.ApiCoreRequests;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Epic("Authorisation cases")
+@Feature("Authorization")
+
 public class UserRegisterTest extends BaseTestCase {
+
+    private final ApiCoreRequests apiCoreRequests = new ApiCoreRequests();
+
     @Test // Пытаемся зарегистрировать пользователя с уже существующим email
     public void testCreateUserWithExistingEmail(){ //Проверияем, что система правильно реагирует на попытку регистрации с уже существующим email
         String email = "vinkotov@example.com";
@@ -48,5 +61,90 @@ public class UserRegisterTest extends BaseTestCase {
         Assertions.assertJsonHasKey(responseCreateAuth, "id"); // проверяем, что в ответе есть поле "id" - сервер создал пользователя и присвоил ему ID
     }
 
+
+    @Test
+    @Description("This test tries to register a user without symbol '@' on email")
+    @DisplayName("Test registration without symbol '@'")
+
+    public void testCreateUserWithInvalidEmail() {
+        String invalidEmail = "learnqablablabla.com";
+        Map<String, String> userData = DataGenerator.getRegistrationData();
+        userData.put("email", invalidEmail);
+
+        Response response = apiCoreRequests.makePostRequest(
+                "https://playground.learnqa.ru/api/user/",
+                userData
+        );
+        //System.out.println(response.asString());
+        //System.out.println(response.statusCode());
+
+        Assertions.assertResponseCodeEquals(response, 400);
+        Assertions.assertResponseTextEquals(
+                response,
+                "Invalid email format"
+
+        );
+
+    }
+
+    @Description("This test without one fields for registration")
+    @DisplayName("Test without one fields for registration")
+    @ParameterizedTest
+    @ValueSource(strings = {"email", "password", "username", "firstName", "lastName"})
+
+    public void testCreateUserWithoutField(String fieldName) {
+        Map<String, String> userData = DataGenerator.getRegistrationData();
+        userData.remove(fieldName);
+
+        Response response = apiCoreRequests.makePostRequest(
+                "https://playground.learnqa.ru/api/user/",
+                userData
+        );
+        //System.out.println(response.statusCode());
+        //System.out.println(response.asString());
+        System.out.println("Отсутствуцюший параметр: " + fieldName);
+
+        Assertions.assertResponseCodeEquals(response, 400);
+        Assertions.assertResponseTextEquals(response, "The following required params are missed: " + fieldName);
+    }
+
+
+    @Test
+    @Description("This test tries to register a user without one symbol on firstName")
+    @DisplayName("Test registration without one symbol on firstName")
+
+    public void testCreateUserWithShortName() {
+        Map<String, String> userData = DataGenerator.getRegistrationData();
+        userData.put("firstName", "x");
+
+        Response response = apiCoreRequests.makePostRequest(
+                "https://playground.learnqa.ru/api/user/",
+                userData
+        );
+        //System.out.println(response.asString());
+        //System.out.println(response.statusCode());
+        Assertions.assertResponseCodeEquals(response, 400);
+        Assertions.assertResponseTextEquals(response, "The value of 'firstName' field is too short");
+    }
+
+    @Test
+    @Description("This test tries to register a user without 250 symbol on firstName")
+    @DisplayName("Test registration without 250 symbol on firstName")
+
+    public void testCreateUserWithLongName() {
+        String longName = "aaaaaaaaaabbbbbbbbbbccccccccccddddddddddeeeeeeeeeeffffffffffgggggggggghhhhhhhhhhiiiiiiiiiijjjjjjjjjjkkkkkkkkkkllllllllllmmmmmmmmmmnnnnnnnnnnooooooooooppppppppppqqqqqqqqqqrrrrrrrrrrssssssssssttttttttttuuuuuuuuuuvvvvvvvvvvwwwwwwwwwwxxxxxxxxxxyyyyyyyyyyz";
+        Map<String, String> userData = DataGenerator.getRegistrationData();
+        userData.put("firstName", longName);
+
+        Response response = apiCoreRequests.makePostRequest(
+                "https://playground.learnqa.ru/api/user/",
+                userData
+        );
+        //System.out.println(response.asString());
+        //System.out.println(response.statusCode());
+
+        Assertions.assertResponseCodeEquals(response, 400);
+        Assertions.assertResponseTextEquals(response, "The value of 'firstName' field is too long");
+    }
 
 }
